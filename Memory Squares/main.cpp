4dc8a187ad9c02ed641b6b4a0b8e1e267acab042
William Armstrong <wkarmstrong03@gmail.com>
2024-04-09 22:21:54 +0100

    Initial Commit

---
+++ Memory Squares/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Memory Squares
+//
+//  Created by William Armstrong on 09/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
